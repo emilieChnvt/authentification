024c1d6df53563854a00e98871135ea7bee9d7f7
Emilie <emilie.chnvt@gmail.com>
2025-01-13 13:32:38 +0100

    verification username existed/ username, password empty

--- verification.php
+++ verification.php
@@ -1,11 +1,35 @@
 <?php
 
+function redirect($message, $page=null)
+{
+    $url = "index.php";
+    if($page)
+    {
+        $url .= "articles.php";
+    }
+    header("Location: $url?message=$message");
+}
 
+$users = [
+    "luc"=> "53f8a9f698d7d2ea9c8a3a6c8d5ab698",
+    "michel"=> "945e9f0b4e381b13aa70b94b89a28709",
+    "eglantine"=> "4fa9239cbfe7d76a31bb46471ce6a976",
+    "patricia"=> "353c8773694fbf1251dec54d98b614a1"
+];
 // si username ou password vide
 if(empty($_POST['username']) || empty($_POST['password'])) {
+    redirect("empty username or password");
 
 }
 $username = $_POST['username'];
 $unEncryptedPassword = $_POST['password'];
+
+
+//si utilisateur existe déjà
+if(isset($users[$username])) {
+    redirect("username already exists");
+}
 $hashedPassword = md5($unEncryptedPassword);
+$users[$username] = $hashedPassword;
+
 
